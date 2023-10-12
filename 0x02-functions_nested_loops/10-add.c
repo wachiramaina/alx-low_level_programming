2baa3488122f99faffba906beb11cb93f797beb5
wachiramaina <wachiravincent254@gmail.com>
2023-10-12 12:56:49 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @c: operand 1
+ * @r: operand 2
+ * Return: result
+ */
+
+int add(int c, int r)
+{
+	int result;
+
+	result = (c + r);
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * times_table -  prints the 9 times table
+ *
+ */
+void times_table(void)
+{
+	int c, r;
+
+	for (c = 0; c < 10; c++)
+	{
+		for (r = 0; r < 10; r++)
+		{
+			t = (c * r);
+
