fcb6ec97151727a2c606458f8d2dfc575d5b039d
wachiramaina <wachiravincent254@gmail.com>
2023-10-12 23:33:52 -0700

    print specific numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9 followed by new line
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && 4)
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
