77e06a28dd14d64744fb4985459c135dcba1c0c8
wachiramaina <wachiravincent254@gmail.com>
2023-10-16 10:36:03 -0700

    battery st98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * @n: pointer to memory address of int parameter
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
