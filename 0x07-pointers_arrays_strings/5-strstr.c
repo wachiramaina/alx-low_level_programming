179c8c92fc2a23de374b83daf3a6883768635fd6
wachiramaina <wachiravincent254@gmail.com>
2023-10-24 18:39:13 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,14 +4,14 @@
 /**
  * _strstr - locates a substring
  * @haystack: string t compare
- * @needles: substring to compare
+ * @needle: substring to compare
  * Return: pointer to beginning of located substring,
  * otherwise NULL
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	for (; *haystack != '\0'; haystack ++)
+	for (; *haystack != '\0'; haystack++)
 	{
 	char *i = haystack;
 	char *j = needle;
