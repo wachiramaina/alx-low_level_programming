7dcd148f3c7dacea64ad92cbf21cc7fa84f0219a
wachiramaina <wachiravincent254@gmail.com>
2023-10-29 04:48:45 -0700

    static libraries task 0

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower -  checks for lowercase character
+ * @c: character to check
+ * Return: 1 if c is lowercase, otherwise 0
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
