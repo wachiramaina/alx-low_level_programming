d18d8d6354b624b15e2eca8f413a1c9720732e0c
wachiramaina <wachiravincent254@gmail.com>
2023-10-29 10:30:24 -0700

    print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument 1
+ * @argv: argument 2
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
